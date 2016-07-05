99377dd843af7274adac6ee4e289020226543b45
necotheone <ntho89@gmail.com>
2016-07-05 12:40:16 +0200

    Cambios en la interfaz de programa

---
+++ RIMEI6.Model/LineaMovil.cs
@@ -0,0 +1,24 @@
+﻿namespace RIMEI6.Model
+{
+    public class LineaMovil
+    {
+        public string imsi { get; set; }
+        public ulong consumoBajada { get; set; }
+        public ulong consumoSubida { get; set; }
+        public ulong consumoTotal { get; set; }
+        public LineaMovil(string imsi, ulong consumoBajada = 0, ulong consumoSubida = 0)
+        {
+            this.imsi = imsi;
+            this.consumoBajada = consumoBajada;
+            this.consumoSubida = consumoSubida;
+            this.consumoTotal = consumoBajada + consumoSubida;
+        }
+
+        public void sumaConsumos(ulong consumoBajada, ulong consumoSubida)
+        {
+            this.consumoBajada += consumoBajada;
+            this.consumoSubida += consumoSubida;
+            this.consumoTotal += consumoBajada + consumoSubida;
+        }
+    }
+}
